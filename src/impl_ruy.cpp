1c08cf41eaf06cbb81d81ab5d5b50c8e797612df
Jerin Philip <jphilip@ed.ac.uk>
2021-11-10 17:14:07 +0000

    Writing out assumptions for missing functions

--- src/impl_ruy.cpp
+++ src/impl_ruy.cpp
@@ -32,11 +32,28 @@ void int8PrepareB(const float *input_B, float scale, float zero_point,
 
 void int8PrepareBFromTransposed(const float *input_B_transposed, float scale,
                                 float zero_point, Index width, Index cols_B,
-                                int8_t *output) {}
+                                int8_t *output) {
+  // Assuming this is just fix things via transpose.
+  std::vector<float> input_B;
+  for (size_t i = 0; i < width; i++) {
+    for (size_t j = 0; j < cols_B; j++) {
+      input_B[i * cols_B + j] = input_B_transposed[j * width + i];
+    }
+  }
+
+  // Now after untranspose, this should be same as calling int8PrepareB
+  int8PrepareB(input_B.data(), scale, zero_point, width, cols_B, output);
+}
 
 void int8PrepareBFromQuantizedTransposed(const int8_t *input_B_quant_transposed,
                                          Index width, Index cols_B,
-                                         int8_t *output) {}
+                                         int8_t *output) {
+  for (size_t i = 0; i < width; i++) {
+    for (size_t j = 0; j < cols_B; j++) {
+      output[i * cols_B + j] = input_B_quant_transposed[j * width + i];
+    }
+  }
+}
 
 void int8PrepareA(const float *input_A, float scale, float zero_point,
                   Index rows_A, Index width, int8_t *output) {
