afeb705f06ecaa8e8fe2bf089da76ab3979a7c48
Jerin Philip <jphilip@ed.ac.uk>
2021-11-17 10:09:17 +0000

    Starting tests on selectColumnsOfB

--- src/firefox_interface_test.cpp
+++ src/firefox_interface_test.cpp
@@ -30,24 +30,43 @@ using namespace pg;
     }                                                                          \
   } while (0)
 
-#define forward(ns, fn)                                                        \
+#define forwardCallToNamespace(ns, fn)                                         \
   template <class... Args> static void fn(Args... args) { ns::fn(args...); }
 
-#define createStruct(ns)                                                       \
+#define namespaceToStructForTemplating(ns)                                     \
   struct _##ns {                                                               \
-    forward(ns, int8PrepareA);                                                 \
-    forward(ns, int8PrepareB);                                                 \
-    forward(ns, int8PrepareBias);                                              \
-    forward(ns, int8MultiplyAndAddBias);                                       \
+    forwardCallToNamespace(ns, int8PrepareA);                                  \
+    forwardCallToNamespace(ns, int8PrepareB);                                  \
+    forwardCallToNamespace(ns, int8PrepareBias);                               \
+    forwardCallToNamespace(ns, int8MultiplyAndAddBias);                        \
+    forwardCallToNamespace(ns, int8SelectColumnsOfB);                          \
   }
 
-createStruct(Intgemm);
-createStruct(Ruy);
+namespaceToStructForTemplating(Intgemm);
+namespaceToStructForTemplating(Ruy);
 
-#define STR(...) STR_(__VA_ARGS__)
-#define STR_(...) #__VA_ARGS__
+void run(std::mt19937_64 &gen64,
+         std::function<void(size_t, size_t, size_t)> f) {
+  constexpr size_t DIM_MAX = 128;
+  constexpr size_t DIM_MIN = 64;
+  constexpr size_t MC_RUNS = 100;
+  for (size_t i = 0; i < MC_RUNS; i++) {
+    std::uniform_int_distribution<> distribution(DIM_MIN, DIM_MAX);
 
-#pragma message "Intgemm: " STR(createStruct(Intgemm))
+    size_t M, N, P; // A = M x N matrix, B = N x P
+
+    M = distribution(gen64);
+    N = distribution(gen64);
+    P = distribution(gen64);
+
+    // Do some stuff to get stuff rounded to multiples of 8
+    const size_t _WIDTH = 64;
+    M = ((M / _WIDTH) + 1) * _WIDTH;
+    N = ((N / _WIDTH) + 1) * _WIDTH;
+    P = ((P / _WIDTH) + 1) * _WIDTH;
+    f(M, N, P);
+  }
+}
 
 // Repeats path for lib with matrices A, B and bias. Final result goes into
 // output, applied with an optional scale.
@@ -82,29 +101,6 @@ void MulABAddBias(Matrix<ElementType> &A, Matrix<ElementType> &B,
                               output);
 }
 
-void run(std::mt19937_64 &gen64,
-         std::function<void(size_t, size_t, size_t)> f) {
-  constexpr size_t DIM_MAX = 128;
-  constexpr size_t DIM_MIN = 64;
-  constexpr size_t MC_RUNS = 100;
-  for (size_t i = 0; i < MC_RUNS; i++) {
-    std::uniform_int_distribution<> distribution(DIM_MIN, DIM_MAX);
-
-    size_t M, N, P; // A = M x N matrix, B = N x P
-
-    M = distribution(gen64);
-    N = distribution(gen64);
-    P = distribution(gen64);
-
-    // Do some stuff to get stuff rounded to multiples of 8
-    const size_t _WIDTH = 64;
-    M = ((M / _WIDTH) + 1) * _WIDTH;
-    N = ((N / _WIDTH) + 1) * _WIDTH;
-    P = ((P / _WIDTH) + 1) * _WIDTH;
-    f(M, N, P);
-  }
-}
-
 TEST(IntgemmVsRuy, NaiveMultiply) {
   std::mt19937_64 gen64;
   gen64.seed(42);
@@ -131,6 +127,45 @@ TEST(IntgemmVsRuy, NaiveMultiply) {
   run(gen64, f);
 }
 
+template <class Lib, class ElementType>
+void MulASelectBAddBias(Matrix<ElementType> &A, Matrix<ElementType> &B,
+                        Matrix<ElementType> &bias, Index *cols_begin,
+                        Index num_cols, ElementType *output,
+                        ElementType output_scale) {
+  intgemm::AlignedVector<int8_t> mA_prepared(A.layout().num_elem()),
+      mB_prepared(B.layout().num_elem());
+  intgemm::AlignedVector<float> mBias_prepared(bias.layout().num_elem());
+
+  DEBUG_MATRIX(A);
+  DEBUG_MATRIX(B);
+  DEBUG_MATRIX(bias);
+  int8_t *A_prepared = mA_prepared.begin();
+  int8_t *B_prepared = mB_prepared.begin();
+  float *bias_prepared = mBias_prepared.begin();
+
+  Layout selected_b_layout(B.nrows(), num_cols, Order::ColMajor);
+  intgemm::AlignedVector<int8_t> mBSelected(selected_b_layout.num_elem());
+  int8_t *B_prepared_selected = mBSelected.begin();
+
+  Lib::int8PrepareA(A.data(), A.scale(), A.zero_point(), A.nrows(), A.ncols(),
+                    A_prepared);
+
+  Lib::int8PrepareB(B.data(), B.scale(), B.zero_point(), B.nrows(), B.ncols(),
+                    B_prepared);
+
+  Lib::int8SelectColumnsOfB(B_prepared, B.nrows(), B.ncols(), cols_begin,
+                            num_cols, B_prepared_selected);
+
+  Lib::int8PrepareBias(B_prepared, A.scale(), A.zero_point(), B.scale(),
+                       B.zero_point(), B.nrows(), B.ncols(), bias.data(),
+                       bias_prepared);
+
+  Lib::int8MultiplyAndAddBias(A_prepared, A.scale(), A.zero_point(),
+                              B_prepared_selected, B.scale(), B.zero_point(),
+                              bias_prepared, output_scale, A.nrows(), A.ncols(),
+                              selected_b_layout.cols(), output);
+}
+
 TEST(IntgemmVsRuy, SelectedMultiply) {
   std::mt19937_64 gen64;
   gen64.seed(42);
@@ -143,24 +178,20 @@ TEST(IntgemmVsRuy, SelectedMultiply) {
     auto B = make_random_matrix<float>(gen64, b_layout, -1.0f, 1.0f);
     auto bias = make_random_matrix<float>(gen64, bias_layout, -1.0f, 1.0f);
 
-    /*
-    std::vector<size_t> cols(b_layout.cols());
+    std::vector<Index> cols(b_layout.cols());
     std::iota(cols.begin(), cols.end(), 0);
-    std::shuffle(cols.begin(), cols.end());
-    // Take first k
+    std::shuffle(cols.begin(), cols.end(), gen64);
     std::uniform_int_distribution<> dist(1, cols.size());
-    size_t cutoff = dist(gen64);
-
-    Layout selected_b_layout(N, cutoff, Order::RowMajor);
-    auto selectedB(selected_b_layout);
-    */
+    Index cutoff = dist(gen64);
 
     float output_scale = 1.0f;
-    Layout productLayout(M, P, Order::RowMajor);
+    Layout productLayout(M, cutoff, Order::RowMajor);
     Matrix<float> intgemmProduct(productLayout);
-    MulABAddBias<_Intgemm>(A, B, bias, intgemmProduct.data(), output_scale);
+    MulASelectBAddBias<_Intgemm>(A, B, bias, cols.data(), cutoff,
+                                 intgemmProduct.data(), output_scale);
     Matrix<float> ruyProduct(productLayout);
-    MulABAddBias<_Ruy>(A, B, bias, ruyProduct.data(), output_scale);
+    MulASelectBAddBias<_Ruy>(A, B, bias, cols.data(), cutoff, ruyProduct.data(),
+                             output_scale);
     float mse = MeanSquaredError(ruyProduct, intgemmProduct);
     DEBUG_MATRIX(ruyProduct);
     DEBUG_MATRIX(intgemmProduct);
