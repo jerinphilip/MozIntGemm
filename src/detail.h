f4045a68394c52523fe9df8e53c1957ff07b3799
Jerin Philip <jerinphilip@live.in>
2021-12-04 22:02:27 +0000

    Adding missing detail.h

---
+++ src/detail.h
@@ -0,0 +1,111 @@
+#include "ruy/system_aligned_alloc.h"
+#include <arm_neon.h>
+#include <cmath>
+#include <cstdint>
+
+namespace pg::Ruy::detail {
+
+template <class T> class AlignedVector {
+public:
+  AlignedVector(size_t num_elem)
+      : size_(num_elem),
+        storage_(reinterpret_cast<T *>(
+            ruy::detail::SystemAlignedAlloc(sizeof(T) * num_elem))) {}
+
+  T *begin() { return storage_; }
+  T *data() { return storage_; }
+  size_t size() const { return size_; }
+  size_t memSize() const { return sizeof(T) * size_; }
+
+  // Forbid copy
+  AlignedVector(const AlignedVector &) = delete;
+  AlignedVector &operator=(const AlignedVector &) = delete;
+
+  ~AlignedVector() {
+    ruy::detail::SystemAlignedFree(reinterpret_cast<void *>(storage_));
+  }
+
+private:
+  T *storage_;
+  size_t size_;
+};
+
+using Index = std::int32_t;
+
+// TODO: Workout similar to ruy. enum value is causing type/value complaints.
+// Intentions are to get the functions to exist together without flouting
+// One-Definition-Rule (ODR).
+struct kStandardCpp {};
+struct kNeon {};
+
+template <class Path> struct Preprocess {
+  static void quantize(const float *input, float scale, float zero_point,
+                       Index rows, Index width, int8_t *output) {
+    const Index size = rows * width;
+    for (size_t i = 0; i < size; i++) {
+      // Round to nearest after multiplying with scale.
+      float value = roundf(scale * input[i]);
+
+      // Since float can store bigger values, we threshold anything that's gone
+      // higher and can't fit in int8.
+      value = std::max(-127.0f, value);
+      value = std::min(127.0f, value);
+
+      // Finally a static cast.
+      output[i] = static_cast<int8_t>(value);
+    };
+  }
+};
+
+template <> struct Preprocess<kNeon> {
+  static void quantize(const float *input, float scale, float zero_point,
+                       Index rows, Index width, int8_t *output) {
+    const float32x4_t *Input = reinterpret_cast<const float32x4_t *>(input);
+    const float32x4_t *InputEnd =
+        reinterpret_cast<const float32x4_t *>(input + rows * width);
+
+    int8x8_t *Output = reinterpret_cast<int8x8_t *>(output);
+    while (Input != InputEnd) {
+      // Vector multiply by scalar
+      // float32x4_t vmulq_n_f32(float32x4_t a, float32_t b);
+      // VMUL.F32 q0,q0,d0[0]
+      float32x4_t scaledFloat_lo = vmulq_n_f32(*Input++, scale);
+
+      // Convert from float
+      // int32x4_t  vcvtnq_s32_f32(float32x4_t a);
+      // VCVT.S32.F32 q0, q0
+      int32x4_t scaledInt_lo = vcvtnq_s32_f32(scaledFloat_lo);
+
+      // Vector saturating narrow integer
+      // int16x4_t  vqmovn_s32(int32x4_t a);   // VQMOVN.S32 d0,q0
+      int16x4_t s16x4_lo = vqmovn_s32(scaledInt_lo);
+
+      // Vector multiply by scalar
+      // float32x4_t vmulq_n_f32(float32x4_t a, float32_t b);
+      // VMUL.F32 q0,q0,d0[0]
+      float32x4_t scaledFloat_hi = vmulq_n_f32(*Input++, scale);
+
+      // Convert from float
+      // int32x4_t  vcvtnq_s32_f32(float32x4_t a);
+      // VCVT.S32.F32 q0, q0
+      int32x4_t scaledInt_hi = vcvtnq_s32_f32(scaledFloat_hi);
+
+      // Vector saturating narrow integer
+      // int16x4_t  vqmovn_s32(int32x4_t a);
+      // VQMOVN.S32 d0,q0
+      int16x4_t s16x4_hi = vqmovn_s32(scaledInt_hi);
+
+      // Combine two ints.
+      // int16x8_t   vcombine_s16(int16x4_t low, int16x4_t high);
+      int16x8_t s16x8 = vcombine_s16(s16x4_lo, s16x4_hi);
+
+      // Vector saturating narrow integer
+      int8x8_t s8x8 = vqmovn_s16(s16x8);
+
+      *Output = s8x8;
+      ++Output;
+    };
+  }
+};
+
+} // namespace pg::Ruy::detail
