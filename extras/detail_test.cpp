abbd0eab0233aa199b5bf8f51298329a358a6331
Jerin Philip <jerinphilip@live.in>
2021-12-07 15:49:52 +0000

    Setting up a checkpoint where tests pass

--- extras/detail_test.cpp
+++ extras/detail_test.cpp
@@ -23,7 +23,7 @@ void Quantize(Matrix<float> &input, Matrix<int8_t> &output) {
                              input.ncols(), output.data());
 }
 
-TEST(PreprocOnARM, NeonVsStandard) {
+TEST(PreprocOnARM, QuantizeNeonVsStandard) {
   std::mt19937_64 gen64;
   const size_t M = 8, N = 64, P = 64;
   auto [A, B, bias] = generateInput(gen64, M, N, P);
@@ -36,6 +36,37 @@ TEST(PreprocOnARM, NeonVsStandard) {
 
   const float MSE_TOLERANCE = 1e-9;
   auto mse = MeanSquaredError(quantizedAStd, quantizedANeon);
+  ASSERT_LT(mse, MSE_TOLERANCE);
+  DEBUG_PRINTABLE(mse);
+}
+
+template <class Path>
+void UnQuantizeAddBias(Matrix<int32_t> &intermediate, Matrix<float> &bias,
+                       Matrix<float> &output) {
+  float unquant_multiplier = 1 / 127.0f;
+  Preprocess<Path>::unquantizeAddBias(intermediate.data(), bias.data(),
+                                      unquant_multiplier, intermediate.nrows(),
+                                      intermediate.ncols(), output.data());
+}
+
+TEST(PreprocOnARM, UnquantizeAddBiasNeonVsStandard) {
+  std::mt19937_64 gen64;
+  const size_t M = 8, N = 64, P = 64;
+  auto [A, B, bias] = generateInput(gen64, M, N, P);
+  Layout productLayout(A.nrows(), B.ncols(), Order::RowMajor);
+  auto intermediate =
+      make_random_matrix<int32_t>(gen64, productLayout, -127, 127);
+
+  Matrix<float> outputStd(productLayout), outputNeon(productLayout);
+
+  UnQuantizeAddBias<kStandardCpp>(intermediate, bias, outputStd);
+  UnQuantizeAddBias<kNeon>(intermediate, bias, outputNeon);
+
+  DEBUG_PRINTABLE(outputStd);
+  DEBUG_PRINTABLE(outputNeon);
+
+  const float MSE_TOLERANCE = 1e-9;
+  auto mse = MeanSquaredError(outputStd, outputNeon);
   ASSERT_LT(mse, MSE_TOLERANCE);
   DEBUG_PRINTABLE(mse);
 }
